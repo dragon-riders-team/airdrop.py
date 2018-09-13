c94777622300d09a0192398fafe4e014d65637eb
root <emmanuel.utrum@pm.me>
2018-09-13 23:49:24 +0000

    added no_repeat.py and example csv file

--- pseudo_snapshot/convert_csv_to_json.sh
+++ pseudo_snapshot/convert_csv_to_json.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
 
-echo "["
-cat $1 \
+string="["
+string+=$(cat $1 \
 	| sed 's/,//g' \
 	| sed 's/^R.*;0\.0000.*//' \
 	| sed '/^\s*$/d' \
 	| perl -lpe 's/(R[\d|\w]+);([\d|\.|\,]+)/  {\n    "addr": "$1",\n    "amount": $2\n  },/' \
-	| sed '$ s/.$//'
-echo "]"
+	| sed '$ s/.$//')
+string+="]"
+
+norepeat_string=`echo $string | ./no_repeat.py`
+
+epoch=`date +%s`
+output="{\"start_time\": $epoch, \"addresses\": "
+output+=$norepeat_string
+output+=',
+  "total": 1,
+  "average": 1,
+  "utxos": 1,
+  "total_addresses": 1,
+  "ignored_addresses": 0,
+  "start_height": 1,
+  "ending_height": 1,
+  "end_time": 1
+}'
+echo $output | jq '.'
 
 exit
 

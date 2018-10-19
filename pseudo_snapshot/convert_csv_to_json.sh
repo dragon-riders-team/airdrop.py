e32c2ec698a3ec5fec05c7eeab935a8c5808ac9d
root <root@electrum1.utrum.io>
2018-10-19 16:39:49 +0000

    fixed bug related to decimals

--- pseudo_snapshot/convert_csv_to_json.sh
+++ pseudo_snapshot/convert_csv_to_json.sh
@@ -5,7 +5,7 @@ addr_array+=$(cat $1 \
 	| sed 's/,//g' \
 	| sed 's/^R.*;0\.0000.*//' \
 	| sed '/^\s*$/d' \
-	| perl -lpe 's/(R[\d|\w]+);([\d|\.|\,]+)/  {\n    "addr": "$1",\n    "amount": $2\n  },/' \
+	| perl -lpe 's/^(R[\d|\w]+);([\d|\,]+(\.\d{4})?)(\d+)?/  {\n    "addr": "$1",\n    "amount": $2\n  },/' \
 	| sed '$ s/.$//')
 addr_array+="]"
 
